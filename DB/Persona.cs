56af884814eea8766f5e30c305c7f133b243cb82
JoaquinBetes <betesjoaquin@gmail.com>
2023-07-14 01:34:46 -0300

    casi primer CRUD

--- DB/Persona.cs
+++ DB/Persona.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Azure;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,5 +24,69 @@ namespace DB
         public string Email { get; set; }
         public DateTime FechaNacimiento { get; set; }
         public int IDPlan { get; set; }
+
+
+        public static async Task<List<Persona>?> getDatosAsync()
+        {
+            // Realizar la solicitud HTTP a la API
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync("http://localhost:5000/api/Persona");
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    List<Persona>? personas = JsonConvert.DeserializeObject<List<Persona>>(responseBody);
+                    return personas;
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error al realizar la solicitud HTTP: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        public static void CreatePersona(int dni, string nombre, string apellido, string telefono, string direccion, string email, DateTime fechaNacimiento, int idPlan) {
+            // Crear una instancia de HttpClient
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // URL del endpoint del controlador Persona
+                string url = "http://localhost:5000/api/Persona";
+
+                // Crear una nueva instancia de Persona con los datos
+                Persona nuevaPersona = new Persona();
+                nuevaPersona.PersonaId = 0;
+                nuevaPersona.DNI = dni;
+                nuevaPersona.Nombre = nombre;
+                nuevaPersona.Apellido = apellido;
+                nuevaPersona.Direccion = direccion;
+                nuevaPersona.Email = email;
+                nuevaPersona.Telefono = telefono;
+                nuevaPersona.FechaNacimiento = fechaNacimiento;
+                nuevaPersona.IDPlan = idPlan;
+                
+                // Serializar la persona en formato JSON
+                string jsonPersona = JsonConvert.SerializeObject(nuevaPersona);
+
+                // Configurar el encabezado y el contenido de la solicitud
+                StringContent content = new StringContent(jsonPersona, Encoding.UTF8, "application/json");
+
+                // Realizar la solicitud HTTP POST
+                HttpResponseMessage response = httpClient.PostAsync(url, content).Result;
+
+                // Verificar el estado de la respuesta
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("La persona se ha insertado correctamente.");
+                }
+                else
+                {
+                    Console.WriteLine("Error al insertar la persona: " + response.StatusCode);
+                }
+            }
+        }
     }
 }
--- UI/FrmPersonas.cs
+++ UI/FrmPersonas.cs
@@ -1,3 +1,6 @@
+using DB;
+using System.Windows.Forms;
+
 namespace UI
 {
     public partial class FrmPersonas : Form
@@ -9,13 +12,21 @@ namespace UI
 
         private void BtnAgregarUsuarios_Click(object sender, EventArgs e)
         {
-            using (var modalForm = new FrmAgregarUsuarios())
+            using (var modalForm = new FrmAgregarPersona())
             {
                 // Mostrar el formulario modal como un diálogo
                 modalForm.ShowDialog();
             }
         }
+        protected override async void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            var personas = await Persona.getDatosAsync();
+            foreach (Persona persona in personas)
+            {
+                DgvPersonas.Rows.Add(persona.DNI, persona.Nombre, persona.Apellido, persona.Telefono, persona.Direccion, persona.Email, persona.FechaNacimiento);
+            }
 
-
+        }
     }
 }
