46d8caf709d1aa9a0dc44a49c0517b48d2852127
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2023-08-07 13:35:28 -0300

    inicio especialidades

--- DB/Especialidad.cs
+++ DB/Especialidad.cs
@@ -5,14 +5,52 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using Entities;
+using System.Net;
 
 namespace DB
 {
     public class Especialidad
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int IdEspecialidad { get; set; }
-        public string Descripcion { get; set; }
+        private string connectionString = "Server=.\\\\SQLEXPRESS;Database=Academia;Trusted_Connection=True;Encrypt=false\"";
+        //[key]
+        //[databasegenerated(databasegeneratedoption.identity)]
+        //public int idespecialidad { get; set; }
+        //public string descripcion { get; set; }
+        public static  List<Entities.Especialidad> Get()
+        {
+            List<Entities.Especialidad> especialidades = new List<Entities.Especialidad>();
+            string sqlQuery = "SELECT * FROM Especialidades";
+            using (SqlConnection connection = new SqlConnection(connectionString)) 
+            {
+                SqlCommand command = new SqlCommand(sqlQuery, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader(); // ya se ejecuto la consulta 
+                    while (reader.Read())
+                    {
+                        
+                        Entities.Especialidad esp = new Entities.Especialidad();
+                        esp.IdEspecialidad = Convert.ToInt32(reader["IdEspecialidad"]);
+                        esp.Descripcion = reader["Discripcion"].ToString();
+                        especialidades.Add(esp);
+                    }
+                    reader.Close();
+                    connection.Close();
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception("Error al cargar la lista de especialidades");
+                }
+
+            }
+
+            return especialidades;
+        }
     }
 }
---
+++ UI/FrmEspecialidadCRUD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DB;
+using System.Windows.Forms;
+namespace UI
+{
+    public partial class frmEspecialidadCRUD : Form
+    {
+        public frmEspecialidadCRUD()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEspecialidadCRUD_Load(object sender, EventArgs e)
+        {
+            load();
+        }
+        private void load()
+        {
+            List<Entities.Especialidad> personas = Especialidad.Get();
+            button2.Text
+
+        }
+
+        private void dgvEspecialidades_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
