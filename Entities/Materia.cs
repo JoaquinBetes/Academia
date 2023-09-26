8a347b48fa07d500be40cffcc1992b3eea04786f
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2023-09-25 21:34:40 -0300

    agregada entidad materia

---
+++ Entities/Materia.cs
@@ -0,0 +1,31 @@
+﻿namespace Entities
+{
+   
+
+    public class Materia
+    {
+        public int MateriaId { get; set; }
+        public string Descripcion { get; set; }
+        public int HsSemanales { get; set; }
+        public int HsTotales { get; set; }
+       
+
+        public Materia()
+        {
+            MateriaId = 0;
+            Descripcion = "-";
+            HsSemanales= 0;
+            HsTotales = 0;
+          
+        }
+
+        public Materia(int materiaId, string descripcion, int hsSemanales, int hsTotales)
+        {
+            MateriaId = materiaId;
+            Descripcion = descripcion;
+            HsSemanales= hsSemanales;
+            HsTotales = hsTotales;
+           
+        }
+    }
+}
